cd3965efcffcde2f924c824d4ded5cfecf482f5e
Pugud19 <noerbayu73@gmail.com>
2022-03-08 14:29:10 +0700

    first commit

--- myscript.js
+++ myscript.js
@@ -1,39 +1,40 @@
-function angka1(){
-    let frm = document.getElementById('formulir');
-    frm.a1.value = '';   
-   }
-   
-   function angka2(){
-       let frm = document.getElementById('formulir');
-       frm.a2.value = '';
-   }
-   function kosongAll(){
-       let frm = document.getElementById('formulir');
-       frm.a1.value = '';   
-       frm.a2.value = '';
-       frm.hasil.value = '';
-   }
+ class Gempa{
+        // member 1 & 2 : variable dan konstruktor
+        constructor(lokasi, skala){
+            this.lokasi = lokasi;
+            this.skala = skala;
 
-   function hitung(aritmatika){
-       let frm = document.getElementById('formulir');
-        a1 = frm.a1.value;
-        a2 = frm.a2.value;
+        }
+        // member fungsi
+        dampak(){
+            // logic fungsi dampak
+            if(this.skala >= 0 && this.skala <= 2) this.gempa = "dampak tidak terasa";
+            else if(this.skala >= 3 && this.skala  <= 4 ) this.gempa = "bangunan retak-retak";
+            else if(this.skala >= 5 && this.skala  <= 6 ) this.gempa = "bangunan Roboh";
+            else if(this.skala >=  7 && this.skala <= 10) this.gempa = "bangunan Roboh dan berpotensi Tsunami";
+            else this.gempa = '';
 
-        // operasi
-        let hsl;
-        if(aritmatika == 'tambah') hsl = parseInt(a1) + parseInt(a2);
-        else if(aritmatika == 'kurang') hsl = parseInt(a1) - parseInt(a2);
-        else if(aritmatika == 'bagi') hsl = parseInt(a1) / parseInt(a2);
-        else if(aritmatika == 'kali') hsl = parseInt(a1) * parseInt(a2);
-        else if(aritmatika == 'pangkat') hsl = Math.pow(parseInt(a1) , parseInt(a2));
-        else hsl = '';
+            document.write(
+                    `Lokasi Gempa : ${this.lokasi}
+                    <br>Skala Gempa : ${this.skala}
+                    <br>Dampak Gempa : ${this.gempa}<hr>`
+                ) 
+        }
 
-    if(isNaN(a1) || a1 == ''){
-        alert("A1 harus diisi berupa number atau tidak boleh kosong");
-    }else if(isNaN(a2) || a2 == ''){
-        alert("A2 harus diisi berupa number atau tidak boleh kosong");
     }
-    else{
-        frm.hasil.value = hsl;  
-    }
-   }
+    // ============ Menciptakan Saldo object ===============
+    let amsterdam = new Gempa('Amsterdam', 6 );
+    let bali = new Gempa('Bali', 5 );
+    let paris = new Gempa('Paris', 9 );
+    let garut = new Gempa('Garut', 8 );
+    let sydney = new Gempa('Sydney', 6 );
+    let ausie = new Gempa('Ausie', 4 );
+    let florida = new Gempa('Florida', 3 );
+    let sanFrancisco = new Gempa('San Francisco', 2 );
+    let london = new Gempa('London', 1 );
+    let rusia = new Gempa('Rusia', 10 );
+
+    // menggunakan member class
+    amsterdam.dampak(); bali.dampak(); paris.dampak(); garut.dampak(); sydney.dampak(); ausie.dampak(); florida.dampak(); sanFrancisco.dampak(); london.dampak(); rusia.dampak();
+
+
